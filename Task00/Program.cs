5b492045ce708cd4d404513695a3ac9f262e3717
Egor <9999966666ivan@gmail.com>
2023-04-23 22:51:33 +0300

    Решил задание 0

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -5,7 +5,7 @@
 // 4 -> 16
 // -3 -> 9
 // -7 -> 49
-Console.Write("Введите число: ");
+Console.Write("Введите число:  ");
 int number = Convert.ToInt32(Console.ReadLine()); //  "7" 
 int square = number * number;
 Console.Write($"Квадрат числа {number} = {square}");
