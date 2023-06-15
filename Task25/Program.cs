ca809e96b73654293091e8b4a7e55a1612c21e94
Egor <9999966666ivan@gmail.com>
2023-06-15 13:56:42 +0300

    Решил задание 25

---
+++ Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
