00fb3668cfb7492eb28c7c9e70e26d9b891d012f
Egor <9999966666ivan@gmail.com>
2023-06-15 18:26:08 +0300

    Решил задание 53

---
+++ Task53/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+ 
+void CreateArray()
+{
+    int[,] array = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    Console.WriteLine("Исходный массив:");
+    PrintArray(array);
+
+    for (int j = 0; j < cols; j++)
+    {
+        int temp = array[0, j];
+        array[0, j] = array[rows - 1, j];
+        array[rows - 1, j] = temp;
+    }
+
+    Console.WriteLine("Измененный массив:");
+    PrintArray(array);
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
