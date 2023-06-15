58b67dd3d74e9ee838efc5e615f03ddfc0d13256
Egor <9999966666ivan@gmail.com>
2023-06-15 14:13:33 +0300

    Решил задание 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,6 +4,14 @@
 
 // 2, 4 -> 16
 
+int Promt (string message)
+{
+    System.Console.Write(message); 
+    string readInput = System.Console.ReadLine ();
+    int result = int.Parse(readInput);
+    return result;
+}
+
 int Power(int powerBase, int exponent)
 {
     int power = 1;
@@ -12,4 +20,21 @@ int Power(int powerBase, int exponent)
         power *= powerBase;
     }
     return power;
+}
+
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int powerBase = Promt("Введите основание: ");
+int exponent = Promt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
 }
